e8b12945e69693e1b1aaa9af492b5a0e73c6804e
Rami Issa <rami.issa.ncd@gmail.com>
2024-11-29 23:41:54 +0300

    Copy Number class

---
+++ Lab 3/Project/Number.cpp
@@ -0,0 +1,132 @@
+#include "Number.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string numberToText(int num) {
+    if (num > 999999999) {
+        throw std::out_of_range("Error: Number exceeds the maximum limit for this function which is 999,999,999");
+    }
+    
+    if (num == 0) return "zero";
+
+    // Text of numbers that are below twenty
+    std::vector<std::string> numbersBelowTwenty = {
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" 
+    };
+
+    // Text of numbers that are tens
+    std::vector<std::string> numbersTens = {
+        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" 
+    };
+
+    std::string number_text = "";
+
+    if (num >= 1000000) {
+        number_text += numberToText(num / 1000000) + " million ";
+        num %= 1000000;        
+    }
+    
+    if (num >= 1000) {
+        number_text += numberToText(num / 1000) + " thousand ";
+        num %= 1000;        
+    }
+
+    if (num >= 100) {
+        number_text += numbersBelowTwenty[num / 100] + " hundred ";
+        num %= 100;
+    }
+
+    if (num >= 20) {
+        number_text += numbersTens[num / 10] + " ";
+        num %= 10;
+    }
+
+    if (num > 0){
+        number_text += numbersBelowTwenty[num];
+    }
+
+    return number_text;
+}
+
+// Constructors
+// Default Constructor
+Number::Number() :value(0), text("zero") { std::cout << "Default Constructor Called" << std::endl; }
+
+// Normal Constructor
+Number::Number(int value) :value(value) {
+    text = numberToText(value);
+    std::cout << "Normal Constructor Called" << std::endl;
+}
+
+// Copy Constructor
+Number::Number(const Number& num) :value(num.value), text(num.text) {
+    std::cout << "Copy Constructor Called" << std::endl;
+}
+
+// Move Constructor
+Number::Number(Number&& num) noexcept {
+    value = std::move(num.value);
+    text = std::move(num.text);
+    std::cout << "Move Constructor Called" << std::endl;
+}
+
+// Assignment Operators
+// Copy Assignment Operator
+Number& Number::operator=(const Number& num) {
+    if (this == &num) return *this;
+
+    // Copy resources
+    value = num.value;
+    text = num.text;
+    
+    std::cout << "Copy Assignment Operator Called" << std::endl;
+    return *this;
+}
+
+// Move Assignment Operator
+Number& Number::operator=(Number&& num) noexcept {
+    if (this == &num) return *this;
+    
+    // Move resources
+    value = num.value;
+    text = std::move(num.text);
+
+    // Reset the moved-from object
+    num.value = 0;
+    num.text = "zero";
+
+    std::cout << "Move Assignment Operator Called" << std::endl;
+    return *this;
+}
+
+// Addition Operators
+// Copy Addition Operator
+Number Number::operator+(const Number& num) const {
+    std::cout << "Copy Addition Operator Called" << std::endl;
+    return Number(value + num.value);
+}
+
+// Move Addition Operator
+Number Number::operator+(Number&& num) const noexcept {
+    int sum = value + num.value;
+
+    // Reset both operands
+    // Reset the first operand (requires const_cast)
+    const_cast<Number*>(this)->value = 0;
+    const_cast<Number*>(this)->text = "zero";
+
+    // Reset the second operand
+    num.value = 0;
+    num.text = "zero";
+
+    std::cout << "Move Addition Operator Called" << std::endl;
+    return Number(sum);
+}
+
+// Destructor
+Number::~Number() {
+    std::cout << "Destructor Called for Number with value = " << value
+              << " and text = \"" << text << "\"" << std::endl;
+}
---
+++ Lab 3/Project/Number.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <utility>
+
+class Number
+{
+    private:
+        int value = 0;
+        std::string text;
+
+    public:
+        // Constructors
+        // Default Constructor
+        Number();
+
+        // Normal Constructor
+        explicit Number(int value);
+
+        // Copy Constructor
+        Number(const Number& num);
+
+        // Move Constructor
+        Number(Number&& num) noexcept;
+
+        // Assignment Operators
+        // Copy Assignment Operator
+        Number& operator=(const Number& num);
+
+        // Move Assignment Operator
+        Number& operator=(Number&& num) noexcept;
+
+        // Addition Operators
+        // Copy Addition Operator
+        Number operator+(const Number& num) const;
+
+        // Move Addition Operator
+        Number operator+(Number&& num) const noexcept;
+
+        // Destructor
+	    ~Number();
+
+        // Setters
+        void setValue(int value) { this->value = value; }
+        void setText(std::string text) { this->text = text; }
+        
+        // Getters
+        [[nodiscard]] int getValue() const { return value; }
+        [[nodiscard]] std::string getText() const { return text; }
+
+        // Friend function to overload `<<` operator
+        friend std::ostream& operator<<(std::ostream& os, const Number& num);
+};
+
+// Implementation of friend operator
+std::ostream& operator<<(std::ostream& os, const Number& num) {
+    os << num.value << " [" << num.text << "]";
+    return os;
+}
