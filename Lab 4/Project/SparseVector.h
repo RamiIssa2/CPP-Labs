beb2dac8a33ad592694aed47b458157262d06068
Rami Issa <rami.issa.ncd@gmail.com>
2024-12-27 17:53:59 +0300

    Defining SparseVector with set and get

---
+++ Lab 4/Project/SparseVector.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include <iterator>
+#include <cstddef>
+
+template <typename T>
+class SparseVector {
+private:
+    std::unordered_map<size_t, T> elements; // Stores non-zero elements
+    size_t size; // Total size of the vector
+
+public:
+    // Constructor
+    SparseVector(size_t size) : size(size) {}
+
+    // Get the size of the vector
+    size_t getSize() const {
+        return size;
+    }
+
+    // Get a value at a specific index (read-only)
+    T get(size_t index) const {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        auto it = elements.find(index);
+        return (it != elements.end()) ? it->second : T(0);
+    }
+
+    // Set a value at a specific index
+    void set(size_t index, const T& value) {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        if (value != T(0)) {
+            elements[index] = value;
+        } else {
+            elements.erase(index); // Remove zero values to maintain sparsity
+        }
+    }
+
+};
